8a5443b0292bae0d89eed2d6f1a674d4a7a09f89
Lean4me2025 <drewhrs2@gmail.com>
2025-08-04 11:00:42 -0700

    Add files via upload

---
+++ NOVA final deploy/script.js
@@ -0,0 +1,10 @@
+
+function showMatches() {
+    const checkedTraits = [...document.querySelectorAll('input[name="trait"]:checked')].map(el => el.value);
+    const results = document.getElementById('results');
+    if (checkedTraits.length === 0) {
+        results.innerHTML = "<p>Please select at least one trait to see matches.</p>";
+    } else {
+        results.innerHTML = "<p>Your selected traits: <strong>" + checkedTraits.join(', ') + "</strong></p>";
+    }
+}
