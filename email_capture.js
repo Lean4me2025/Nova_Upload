5e3331b056cbcb5efaa14e33302c792697948567
Lean4me2025 <drewhrs2@gmail.com>
2025-08-05 16:45:20 -0700

    Delete email_capture.js

--- email_capture.js
+++
@@ -1,9 +0,0 @@
-
-function saveEmail() {
-  const emailInput = document.getElementById("email");
-  const email = emailInput.value;
-  if (email) {
-    localStorage.setItem("nova_user_email", email);
-    alert("Email saved for updates!");
-  }
-}
